9f1a8f0ee3e2a4fde60005556108606b7809cf1b
vks <vyshnavks85@gmail.com>
2023-02-25 21:47:08 +0530

    User list active, Inactive, bug fixes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,5 @@
-import SideNavbar from "../components/SideNavbar";
 import DashboardHeader from "../components/DashboardHeader";
 import { Outlet, Route, Routes } from "react-router-dom";
-import Admin from "../components/Admin";
 
 const Dashboard = () => {
   return (
