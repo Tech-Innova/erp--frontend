073d916de983bbde1f5c4f5f7b63861b067d0cd3
superRaptor911 <raptor.inc2018@gmail.com>
2023-02-27 15:53:40 +0530

    api list user

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,8 +1,9 @@
 import {
+  TUserAuthModel,
   TUserLoginRequest,
   TUserSignupRequest,
 } from "@super_raptor911/erp-types";
-import { postRequest, getJson } from "./request";
+import { postRequest, getJson, getRequest } from "./request";
 
 const server: string = import.meta.env.VITE_API_SERVER;
 console.log("server", server);
@@ -35,4 +36,17 @@ export const api_loginUser = async (userInfo: TUserLoginRequest) => {
     alert(error);
   }
   return false;
+};
+
+export const api_listUsers = async () => {
+  try {
+    const res = await getRequest(server + "user/list");
+    if (res.status != 200) {
+      throw (await getJson(res)).message;
+    }
+    return (await getJson(res)) as TUserAuthModel[];
+  } catch (error) {
+    console.error("users::api_loginUser ", error);
+    alert(error);
+  }
 };
